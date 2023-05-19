d53302f5b3b830a47b579c22f48fd3dce750aea2
Lakshmi Suma Divvela <lakshmi.suma@ibm.com>
2023-05-19 12:43:08 +0530

    first

--- main.tf
+++ main.tf
@@ -71,7 +71,7 @@ resource "ibm_container_vpc_cluster" "testcluster1" {
   flavor            = "bx2.4x16"
   worker_count      = 2
   resource_group_id=var.resource_group_id
-  kube_version      = "1.26.4"  
+  kube_version      = "1.25.9"  
   update_all_workers     = true
   wait_for_worker_update = true
   depends_on = [ ibm_is_subnet.subnet4 ,data.ibm_container_vpc_cluster.cluster ,local.previous]
@@ -82,51 +82,51 @@ resource "ibm_container_vpc_cluster" "testcluster1" {
   }
 }
 
-data "ibm_container_vpc_cluster" "cluster1" {
-  name  = "testcluster1"
-  depends_on = [ ibm_container_vpc_cluster.testcluster1 ]
+# data "ibm_container_vpc_cluster" "cluster1" {
+#   name  = "testcluster1"
+#   depends_on = [ ibm_container_vpc_cluster.testcluster1 ]
   
-}
-# Print the id's of the workers
-locals {
-  value2 = data.ibm_container_vpc_cluster.cluster1.workers
-  # depends_on = [ data.ibm_container_vpc_cluster.cluster1 ]
+# }
+# # Print the id's of the workers
+# locals {
+#   value2 = data.ibm_container_vpc_cluster.cluster1.workers
+#   # depends_on = [ data.ibm_container_vpc_cluster.cluster1 ]
   
-}
-data "ibm_container_vpc_cluster_worker" "worker1" {
-  for_each= toset(local.value2)
-  worker_id = each.value
-  cluster_name_id = "testcluster1"
-  depends_on = [ data.ibm_container_vpc_cluster.cluster1 ]
+# }
+# data "ibm_container_vpc_cluster_worker" "worker1" {
+#   for_each= toset(local.value2)
+#   worker_id = each.value
+#   cluster_name_id = "testcluster1"
+#   depends_on = [ data.ibm_container_vpc_cluster.cluster1 ]
 
 
-}
+# }
 
-locals {
-  # depends_on = [ data.ibm_container_vpc_cluster_worker.worker1 ]
-  new = [
-    for i in data.ibm_container_vpc_cluster.cluster1.workers:
-    lookup(lookup(lookup(data.ibm_container_vpc_cluster_worker.worker1,i),"network_interfaces")[0],"ip_address")
+# locals {
+#   # depends_on = [ data.ibm_container_vpc_cluster_worker.worker1 ]
+#   new = [
+#     for i in data.ibm_container_vpc_cluster.cluster1.workers:
+#     lookup(lookup(lookup(data.ibm_container_vpc_cluster_worker.worker1,i),"network_interfaces")[0],"ip_address")
     
-  ]
+#   ]
   
-}
+# }
 
-output "ip2" {
-  value = local.new
+# output "ip2" {
+#   value = local.new
   
   
   
-}
+# }
 
-locals {
-  # validation{
-  #   condition=local.value1==local.value2
-  #   error_message="Please chane the ip_address in the bluefringe"
-  # }
-  display=local.previous!=local.new?1:0
-}
-output "display" {
-  value = local.display
+# locals {
+#   # validation{
+#   #   condition=local.value1==local.value2
+#   #   error_message="Please chane the ip_address in the bluefringe"
+#   # }
+#   display=local.previous!=local.new?1:0
+# }
+# output "display" {
+#   value = local.display
   
-}
+# }
