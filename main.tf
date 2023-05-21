9ac94c4482f1170a8c2ed45a86f0ebdfb96e9eee
Lakshmi Suma Divvela <lakshmi.suma@ibm.com>
2023-05-21 18:07:17 +0530

    first

--- main.tf
+++ main.tf
@@ -34,107 +34,107 @@ data "ibm_container_vpc_cluster" "cluster" {
 }
 
 
-data "ibm_container_vpc_cluster_worker" "worker" {
-  for_each= toset(data.ibm_container_vpc_cluster.cluster.workers)
-  worker_id = each.value
-  cluster_name_id = "testcluster1"
-  depends_on = [ data.ibm_container_vpc_cluster.cluster ]
+# data "ibm_container_vpc_cluster_worker" "worker" {
+#   for_each= toset(data.ibm_container_vpc_cluster.cluster.workers)
+#   worker_id = each.value
+#   cluster_name_id = "testcluster1"
+#   depends_on = [ data.ibm_container_vpc_cluster.cluster ]
 
 
-}
+# }
 
-locals {
-  # depends_on = [ data.ibm_container_vpc_cluster_worker.worker ]
-  previous = [
-    for i in data.ibm_container_vpc_cluster.cluster.workers:
-    lookup(lookup(lookup(data.ibm_container_vpc_cluster_worker.worker,i),"network_interfaces")[0],"ip_address")
-    
-  ]
-  
-}
 # locals {
-#   ip1=data.ibm_container_vpc_cluster.cluster.workers
+#   # depends_on = [ data.ibm_container_vpc_cluster_worker.worker ]
+#   previous = [
+#     for i in data.ibm_container_vpc_cluster.cluster.workers:
+#     lookup(lookup(lookup(data.ibm_container_vpc_cluster_worker.worker,i),"network_interfaces")[0],"ip_address")
+    
+#   ]
   
 # }
-output "prev" {
-  value=local.previous
-  
-}
-# # Print the id's of the workers
-# # locals  {
-# #   value1 = data.ibm_container_vpc_cluster.cluster.workers
-# #   depends_on = [ data.ibm_container_vpc_cluster.cluster ]
+# # locals {
+# #   ip1=data.ibm_container_vpc_cluster.cluster.workers
   
 # # }
-# output "ip1" {
-#   value = local.previous
-#   depends_on = [ local.previous ]
+# output "prev" {
+#   value=local.previous
   
 # }
+# # # Print the id's of the workers
+# # # locals  {
+# # #   value1 = data.ibm_container_vpc_cluster.cluster.workers
+# # #   depends_on = [ data.ibm_container_vpc_cluster.cluster ]
+  
+# # # }
+# # output "ip1" {
+# #   value = local.previous
+# #   depends_on = [ local.previous ]
+  
+# # }
 
 
-resource "ibm_container_vpc_cluster" "testcluster1" {
-  name              = "testcluster1"
-  vpc_id            = ibm_is_vpc.vpc2.id
-  flavor            = "bx2.4x16"
-  worker_count      = 2
-  resource_group_id=var.resource_group_id
-  kube_version      = "1.25.9"  
-  update_all_workers     = true
-  wait_for_worker_update = true
-  depends_on = [ ibm_is_subnet.subnet4,data.ibm_container_vpc_cluster.cluster,data.ibm_container_vpc_cluster_worker.worker]
-  #  ,data.ibm_container_vpc_cluster.cluster ,local.previous]
-  zones {
-    subnet_id = ibm_is_subnet.subnet4.id
-    name      = "us-south-1"
+# resource "ibm_container_vpc_cluster" "testcluster1" {
+#   name              = "testcluster1"
+#   vpc_id            = ibm_is_vpc.vpc2.id
+#   flavor            = "bx2.4x16"
+#   worker_count      = 2
+#   resource_group_id=var.resource_group_id
+#   kube_version      = "1.25.9"  
+#   update_all_workers     = true
+#   wait_for_worker_update = true
+#   depends_on = [ ibm_is_subnet.subnet4,data.ibm_container_vpc_cluster.cluster,data.ibm_container_vpc_cluster_worker.worker]
+#   #  ,data.ibm_container_vpc_cluster.cluster ,local.previous]
+#   zones {
+#     subnet_id = ibm_is_subnet.subnet4.id
+#     name      = "us-south-1"
     
-  }
-}
-#To fetch information about the vpc cluster
-data "ibm_container_vpc_cluster" "cluster1" {
-  name  = "testcluster1"
-  depends_on = [ ibm_container_vpc_cluster.testcluster1,data.ibm_container_vpc_cluster.cluster ]
+#   }
+# }
+# #To fetch information about the vpc cluster
+# data "ibm_container_vpc_cluster" "cluster1" {
+#   name  = "testcluster1"
+#   depends_on = [ ibm_container_vpc_cluster.testcluster1,data.ibm_container_vpc_cluster.cluster ]
   
-}
-# Print the id's of the workers
+# }
+# # Print the id's of the workers
 
 
-locals {
-  my_map = { for idx in data.ibm_container_vpc_cluster.cluster1.workers : idx => "dynamic_value_${idx}" }
-}
-# l "workers" {
-#   value = data.ibm_container_vpc_cluster.cluster1.workers
-#   depends_on = [ data.ibm_container_vpc_cluster.cluster1 ]
+# locals {
+#   my_map = { for idx in data.ibm_container_vpc_cluster.cluster1.workers : idx => "dynamic_value_${idx}" }
+# }
+# # l "workers" {
+# #   value = data.ibm_container_vpc_cluster.cluster1.workers
+# #   depends_on = [ data.ibm_container_vpc_cluster.cluster1 ]
+  
+# # }
+
+# #To fetch information about each worker node
+# data "ibm_container_vpc_cluster_worker" "worker1" {
+#   depends_on = [ ibm_container_vpc_cluster.testcluster1,data.ibm_container_vpc_cluster.cluster1 ]
+#   # for_each= toset(data.ibm_container_vpc_cluster.cluster1.workers)
+#   count = length(data.ibm_container_vpc_cluster.cluster1.workers)
+#   # for_each = local.my_map
+#   worker_id = data.ibm_container_vpc_cluster.cluster1.workers[count.index]
+#   cluster_name_id = "testcluster1"
   
 # }
 
-#To fetch information about each worker node
-data "ibm_container_vpc_cluster_worker" "worker1" {
-  depends_on = [ ibm_container_vpc_cluster.testcluster1,data.ibm_container_vpc_cluster.cluster1 ]
-  # for_each= toset(data.ibm_container_vpc_cluster.cluster1.workers)
-  count = length(data.ibm_container_vpc_cluster.cluster1.workers)
-  # for_each = local.my_map
-  worker_id = data.ibm_container_vpc_cluster.cluster1.workers[count.index]
-  cluster_name_id = "testcluster1"
-  
-}
+# #To print the information about the workers
+# # output "ip_address" {
+# #   value=data.ibm_container_vpc_cluster_worker.worker1
+# #   depends_on = [ data.ibm_container_vpc_cluster_worker.worker1 ]
+# # }
 
-#To print the information about the workers
-# output "ip_address" {
-#   value=data.ibm_container_vpc_cluster_worker.worker1
-#   depends_on = [ data.ibm_container_vpc_cluster_worker.worker1 ]
-# }
-
-#To filter the ip address and store in a list
-output "ip" {
-  depends_on = [ ibm_container_vpc_cluster.testcluster1 ]
-  value = [
-    for i in data.ibm_container_vpc_cluster.cluster1.workers:
-    lookup(lookup(lookup(data.ibm_container_vpc_cluster_worker.worker1,i),"network_interfaces")[0],"ip_address")
+# #To filter the ip address and store in a list
+# output "ip" {
+#   depends_on = [ ibm_container_vpc_cluster.testcluster1 ]
+#   value = [
+#     for i in data.ibm_container_vpc_cluster.cluster1.workers:
+#     lookup(lookup(lookup(data.ibm_container_vpc_cluster_worker.worker1,i),"network_interfaces")[0],"ip_address")
     
-  ]
+#   ]
   
-}
+# }
 
 # data "ibm_container_vpc_cluster_worker" "worker2" {
 #   worker_id       = "kube-cgnsv2ud0jhkn4p263d0-testcluster-mywp-00000761"
