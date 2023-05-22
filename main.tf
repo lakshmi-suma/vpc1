00a2cb0e5e65d081e08f7ca428bc0c5ed629e167
Lakshmi Suma Divvela <lakshmi.suma@ibm.com>
2023-05-22 11:07:37 +0530

    first

--- main.tf
+++ main.tf
@@ -65,39 +65,57 @@ output "workers" {
   depends_on = [ data.ibm_container_vpc_cluster.cluster1 ]
   
 }
-locals {
-  ids=data.ibm_container_vpc_cluster.cluster1.workers
-}
 
+module "kubectl" {
+  source  = "magnolia-sre/kubectl-cmd/kubernetes"
 
-
-#To fetch information about each worker node
-data "ibm_container_vpc_cluster_worker" "worker1" {
-  # count = length(local.ids)
-  # # name               = "diag-rule"
-  # worker_id = local.ids[count.index]
-  for_each= local.ids
-  worker_id = each.value
-  cluster_name_id = "test-cluster1"
-  depends_on = [ ibm_container_vpc_cluster.cluster,local.ids ]
-}
-
-#To print the information about the workers
-output "ip_address" {
-  value=data.ibm_container_vpc_cluster_worker.worker1
-  depends_on = [ data.ibm_container_vpc_cluster_worker.worker1 ]
-}
-
-#To filter the ip address and store in a list
-output "ip" {
-  depends_on = [ data.ibm_container_vpc_cluster_worker.worker1 ]
-  value = [
-    for i in data.ibm_container_vpc_cluster.cluster1.workers:
-    lookup(lookup(lookup(data.ibm_container_vpc_cluster_worker.worker1,i),"network_interfaces")[0],"ip_address")
-    
+  app            = "myapp"
+  cluster-name   = "test-cluster1"
+  # credentials    = {
+  #   kubeconfig-path: "./mykubeconfigfile"
+  # }
+  cmds           = [ <<-EOT
+    kubectl get nodes
+     
+EOT
+    # ,
+    # "kubectl apply -f any.yaml"
   ]
-  
 }
+
+# locals {
+#   ids=data.ibm_container_vpc_cluster.cluster1.workers
+# }
+
+
+
+# #To fetch information about each worker node
+# data "ibm_container_vpc_cluster_worker" "worker1" {
+#   # count = length(local.ids)
+#   # # name               = "diag-rule"
+#   # worker_id = local.ids[count.index]
+#   for_each= local.ids
+#   worker_id = each.value
+#   cluster_name_id = "test-cluster1"
+#   depends_on = [ ibm_container_vpc_cluster.cluster,local.ids ]
+# }
+
+# #To print the information about the workers
+# output "ip_address" {
+#   value=data.ibm_container_vpc_cluster_worker.worker1
+#   depends_on = [ data.ibm_container_vpc_cluster_worker.worker1 ]
+# }
+
+# #To filter the ip address and store in a list
+# output "ip" {
+#   depends_on = [ data.ibm_container_vpc_cluster_worker.worker1 ]
+#   value = [
+#     for i in data.ibm_container_vpc_cluster.cluster1.workers:
+#     lookup(lookup(lookup(data.ibm_container_vpc_cluster_worker.worker1,i),"network_interfaces")[0],"ip_address")
+    
+#   ]
+  
+# }
 
 
 # data "ibm_container_vpc_cluster_worker" "worker" {
