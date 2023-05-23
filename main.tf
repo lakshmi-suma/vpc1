1b655a7b7b6227f1f32358a2affc371c01bbdb0d
Lakshmi Suma Divvela <lakshmi.suma@ibm.com>
2023-05-23 14:01:27 +0530

    first commit

--- main.tf
+++ main.tf
@@ -48,8 +48,7 @@ resource "ibm_container_vpc_cluster" "cluster5" {
   kube_version      = "1.25.9"  
   update_all_workers     = true
   wait_for_worker_update = true
-  depends_on = [ ibm_is_subnet.subnet4]
-  # ,data.ibm_container_vpc_cluster.cluster ]
+  depends_on = [ ibm_is_subnet.subnet4,data.ibm_container_vpc_cluster.cluster ]
   zones {
     subnet_id = ibm_is_subnet.subnet4.id
     name      = "us-south-1"
