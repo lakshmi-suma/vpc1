8b0381cc53355b1ee328c9020a44d19fbc9345e3
Lakshmi Suma Divvela <lakshmi.suma@ibm.com>
2023-05-23 14:00:43 +0530

    first commit

--- main.tf
+++ main.tf
@@ -27,16 +27,16 @@ resource "ibm_is_subnet_public_gateway_attachment" "subatt1" {
   public_gateway         = ibm_is_public_gateway.gateway1.id
 }
 
-# data "ibm_container_vpc_cluster" "cluster" {
-#   name  = "test-cluster1"
-#   # depends_on = [ ibm_container_vpc_cluster.cluster ]
+data "ibm_container_vpc_cluster" "cluster" {
+  name  = "test-cluster1"
+  # depends_on = [ ibm_container_vpc_cluster.cluster ]
   
-# }
-# locals {
-#   value1= data.ibm_container_vpc_cluster.cluster.workers
-#   # depends_on = [ data.ibm_container_vpc_cluster.cluster ]
+}
+locals {
+  value1= data.ibm_container_vpc_cluster.cluster.workers
+  # depends_on = [ data.ibm_container_vpc_cluster.cluster ]
   
-# }
+}
 
 
 resource "ibm_container_vpc_cluster" "cluster5" {
@@ -45,7 +45,7 @@ resource "ibm_container_vpc_cluster" "cluster5" {
   flavor            = "bx2.4x16"
   worker_count      = 2
   resource_group_id=var.resource_group_id
-  kube_version      = "1.24.13"  
+  kube_version      = "1.25.9"  
   update_all_workers     = true
   wait_for_worker_update = true
   depends_on = [ ibm_is_subnet.subnet4]
@@ -60,36 +60,36 @@ resource "ibm_container_vpc_cluster" "cluster5" {
 
 
 
-# #To fetch information about the vpc cluster
-# data "ibm_container_vpc_cluster" "cluster1" {
-#   name  = "test-cluster1"
-#   depends_on = [ ibm_container_vpc_cluster.cluster5 ]
+#To fetch information about the vpc cluster
+data "ibm_container_vpc_cluster" "cluster1" {
+  name  = "test-cluster1"
+  depends_on = [ ibm_container_vpc_cluster.cluster5 ]
   
-# }
-# # Print the id's of the workers
-# locals  {
-#   value2 = data.ibm_container_vpc_cluster.cluster1.workers
-#   # depends_on = [ data.ibm_container_vpc_cluster.cluster1 ]
+}
+# Print the id's of the workers
+locals  {
+  value2 = data.ibm_container_vpc_cluster.cluster1.workers
+  # depends_on = [ data.ibm_container_vpc_cluster.cluster1 ]
   
-# }
+}
 
-# locals {
-#   a=local.value1!=local.value2?"Please change the ips in the bluefringe":"The ips are upto date"
-# }
+locals {
+  a=local.value1!=local.value2?"Please change the ips in the bluefringe":"The ips are upto date"
+}
 
-# output "old_ids" {
-#   value = local.value1
+output "old_ids" {
+  value = local.value1
   
-# }
-# output "new_ids" {
-#   value=local.value2
+}
+output "new_ids" {
+  value=local.value2
   
-# }
+}
 
-# output "Alert" {
-#  value = local.a
+output "Alert" {
+ value = local.a
   
-# }
+}
 
 
 
