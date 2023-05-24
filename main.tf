51d514a23b2bc5a8c43a018b9c5f03ad2e7e197b
Lakshmi Suma Divvela <lakshmi.suma@ibm.com>
2023-05-24 13:03:14 +0530

    first commit

--- main.tf
+++ main.tf
@@ -46,22 +46,22 @@ resource "ibm_is_subnet_public_gateway_attachment" "subatt1" {
 
 
 
-# resource "ibm_container_vpc_cluster" "cluster5" {
-#   name              = "test-cluster1"
-#   vpc_id            = ibm_is_vpc.vpc2.id
-#   flavor            = "bx2.4x16"
-#   worker_count      = 2
-#   resource_group_id=var.resource_group_id
-#   kube_version      = "1.26.4"  
-#   update_all_workers     = true
-#   wait_for_worker_update = true
-#   depends_on = [ ibm_is_subnet.subnet4,data.ibm_container_vpc_cluster.cluster ]
-#   zones {
-#     subnet_id = ibm_is_subnet.subnet4.id
-#     name      = "us-south-1"
+resource "ibm_container_vpc_cluster" "cluster5" {
+  name              = "test-cluster1"
+  vpc_id            = ibm_is_vpc.vpc2.id
+  flavor            = "bx2.4x16"
+  worker_count      = 2
+  resource_group_id=var.resource_group_id
+  kube_version      = "1.24.13"  
+  update_all_workers     = true
+  wait_for_worker_update = true
+  depends_on = [ ibm_is_subnet.subnet4,data.ibm_container_vpc_cluster.cluster ]
+  zones {
+    subnet_id = ibm_is_subnet.subnet4.id
+    name      = "us-south-1"
     
-#   }
-# }
+  }
+}
 
 
 
